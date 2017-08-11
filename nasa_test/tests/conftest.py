028f43eec1bd3f9808e93b310db29be107cb131b
ktyshchenko <kostya.tyshchenko@gmail.com>
2017-08-11 12:14:20 +0300

    done

---
+++ nasa_test/tests/conftest.py
@@ -0,0 +1,3 @@
+"""
+This file is needed for future paramtrization of tests
+"""
---
+++ nasa_test/wrappers/base_page.py
@@ -0,0 +1,7 @@
+class BasePage(object):
+    """
+
+    Base class to initialize the base page that will be called from all wrappers
+
+    """
+    pass
